f4481234f48e600fd93e38eb3b83ca33dfc0d6fb
aurumcodex <syberfox.64@gmail.com>
2018-03-06 13:44:00 -0500

    changed some script things

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,12 @@
 // Author: Nathan Adams
 function changeColor() {
-  document.getElementsByClassName("data").style.borderColor = "#8db8d4";
+  // document.getElementsByClassName("data").style.borderColor = "#8db8d4";
+  document.getElementsByTagName("td").style.borderColor = "#8db8d4";
+  document.getElementsByTagName("th").style.borderColor = "#8db8d4";
 }
 
 function resetColor() {
-  document.getElementsByClassName("data").style.borderColor = "#353d42";
+  // document.getElementsByClassName("data").style.borderColor = "#353d42";
+  document.getElementsByTagName("td").style.borderColor = "#8db8d4";
+  document.getElementsByTagName("th").style.borderColor = "#8db8d4";
 }
