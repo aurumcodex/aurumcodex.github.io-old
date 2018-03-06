b82b8e1f47252e2c9dcbd258bd63ea8255532e92
aurumcodex <syberfox.64@gmail.com>
2018-03-06 13:35:56 -0500

    testing a thing

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
 // Author: Nathan Adams
 function changeColor() {
-  document.getElementById("tableField").style.borderColor = "#8db8d4";
+  document.getElementByClassName("data").style.borderColor = "#8db8d4";
 }
 
 function resetColor() {
-  document.getElementById("tableField").style.borderColor = "#353d42";
+  document.getElementByClassName("data").style.borderColor = "#353d42";
 }
