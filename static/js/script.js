dde2da6f3703601cd4645e90e5ab45f265959b81
aurumcodex <syberfox.64@gmail.com>
2018-03-06 14:20:50 -0500

    changed some other stuff in the files

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ function changeColor() {
   // document.getElementsByClassName("data").style.borderColor = "#8db8d4";
   // document.getElementsByTagName("td").style.borderColor = "#8db8d4";
   // document.getElementsByTagName("th").style.borderColor = "#8db8d4";
-  document.getElementById("formy").style.borderColor = "#353d42";
+  document.getElementById("formy").style.borderColor = "#8db8d4";
 }
 
 function resetColor() {
