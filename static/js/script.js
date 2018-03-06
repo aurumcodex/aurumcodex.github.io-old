c1eee1c9ba588b563f78cfb376fe7943414ce5c3
aurumcodex <syberfox.64@gmail.com>
2018-03-06 14:12:35 -0500

    more stuff changed

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,12 @@ function changeColor() {
   // document.getElementsByClassName("data").style.borderColor = "#8db8d4";
   // document.getElementsByTagName("td").style.borderColor = "#8db8d4";
   // document.getElementsByTagName("th").style.borderColor = "#8db8d4";
-  document.getElementById("tableField").style.borderColor = "#353d42";
+  document.getElementById("formy").style.borderColor = "#353d42";
 }
 
 function resetColor() {
   // document.getElementsByClassName("data").style.borderColor = "#353d42";
   // document.getElementsByTagName("td").style.borderColor = "#8db8d4";
   // document.getElementsByTagName("th").style.borderColor = "#8db8d4";
-  document.getElementById("tableField").style.borderColor = "#0e3b4f";
+  document.getElementById("formy").style.borderColor = "#0e3b4f";
 }
