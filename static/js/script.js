044531d150f23e69289f2b136b109cdd1c12592f
aurumcodex <syberfox.64@gmail.com>
2018-03-06 13:37:57 -0500

    forgot a letter; added it

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
 // Author: Nathan Adams
 function changeColor() {
-  document.getElementByClassName("data").style.borderColor = "#8db8d4";
+  document.getElementsByClassName("data").style.borderColor = "#8db8d4";
 }
 
 function resetColor() {
-  document.getElementByClassName("data").style.borderColor = "#353d42";
+  document.getElementsByClassName("data").style.borderColor = "#353d42";
 }
