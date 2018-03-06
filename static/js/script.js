5d91c24967470aa42ef6d850e6de18888eb1485a
aurumcodex <syberfox.64@gmail.com>
2018-03-06 13:58:58 -0500

    testing a table thing

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,14 @@
 // Author: Nathan Adams
 function changeColor() {
   // document.getElementsByClassName("data").style.borderColor = "#8db8d4";
-  document.getElementsByTagName("td").style.borderColor = "#8db8d4";
-  document.getElementsByTagName("th").style.borderColor = "#8db8d4";
+  // document.getElementsByTagName("td").style.borderColor = "#8db8d4";
+  // document.getElementsByTagName("th").style.borderColor = "#8db8d4";
+  document.getElementById("tableField").style.borderColor = "#8db8d4";
 }
 
 function resetColor() {
   // document.getElementsByClassName("data").style.borderColor = "#353d42";
-  document.getElementsByTagName("td").style.borderColor = "#8db8d4";
-  document.getElementsByTagName("th").style.borderColor = "#8db8d4";
+  // document.getElementsByTagName("td").style.borderColor = "#8db8d4";
+  // document.getElementsByTagName("th").style.borderColor = "#8db8d4";
+  document.getElementById("tableField").style.borderColor = "#353d42";
 }
