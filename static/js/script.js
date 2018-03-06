38380c9287bc46f9617f3835ecfcf7e7d5180ec4
aurumcodex <syberfox.64@gmail.com>
2018-03-06 13:18:32 -0500

    changed some stuff yet again

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// Author: Nathan Adams
+function changeColor() {
+  document.getElementById("tableField").style.borderColor = "#8db8d4";
+}
+
+function resetColor() {
+  document.getElementById("tableField").style.borderColor = "#353d42";
+}
