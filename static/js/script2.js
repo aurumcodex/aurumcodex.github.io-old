262369bfda45d47bb8219a19492753ba58ec366a
aurumcodex <syberfox.64@gmail.com>
2018-03-31 21:55:13 -0400

    needed to add these things

---
+++ static/js/script2.js
@@ -0,0 +1,11 @@
+function printMe() {
+	var xhttpr = new XMLHttpRequest();
+	xhttpr.onreadystatechange = function() {
+		//if (this.readyState == 4 && this.status == 200) {
+		//	document.getElementById("printMe").innerHTML = this.responseText;
+		//}
+		(this.readyState == 4 && this.status == 200) ? document.getElementById("printMe").innerHTML = this.responseText; : console.log("an error has occured");
+	};
+	xhttpr.open("GET", "./misc/stuff.txt", true);
+	xhttpr.send();
+}
