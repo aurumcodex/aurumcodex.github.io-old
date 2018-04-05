cbe374dbabab94f03db75983097e48aa9c8eb9ce
aurumcodex <syberfox.64@gmail.com>
2018-04-05 15:33:24 -0400

    misspelt "body"; fixed

--- static/js/three_use.js
+++ static/js/three_use.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.bodu.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 var geometry = new THREE.BoxGeometry(1,1,1);
 var meterial = new THREE.MeshBasicMaterial({color: 0x90dbb9});
 var cube = new THREE.Mesh(geometry, material);
