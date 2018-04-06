527a621e8a67726c3b0f8e844e2fd26f47a87572
aurumcodex <syberfox.64@gmail.com>
2018-04-06 10:03:28 -0400

    testing paperJS rectangles

--- static/js/three_use.js
+++ static/js/three_use.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth-20, window.innerHeight-20);
 document.body.appendChild(renderer.domElement);
 var geometry = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial({color: 0x90dbb9});
